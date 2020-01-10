8f6406cf7d8c40ed83eef891e691652b2158e7ee
nikita.balakirev <balakirevnikita@yandex.ru>
2020-01-10 13:02:12 +0300

    feat(): added copy-webpack-plugin

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import galleria from 'galleria';
 window.Galleria = galleria;
 window.jQuery = $;
 
-Galleria.loadTheme('node_modules/galleria/dist/themes/classic/galleria.classic.js');
+Galleria.loadTheme('assets/galleria.classic.js');
 Galleria.run('.galleria');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -30,6 +31,10 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'index.html')
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: 'node_modules/galleria/dist/themes/classic/galleria.classic.min.js', to: 'assets/galleria.classic.js' },
+            { from: 'node_modules/galleria/dist/themes/classic/galleria.classic.min.css', to: 'assets/galleria.classic.css' },
+        ])
     ]
 };
